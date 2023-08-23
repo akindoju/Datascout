8dc0b1dea6ea138ea77d34f978ab5c5b085aab0c
Olusola Akindoju <davidd.akindoju@gmail.com>
2023-08-23 18:51:04 +0100

    Added profile to navbar

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,22 +1,29 @@
-import { FC, SetStateAction } from "react";
+import { FC, SetStateAction, useState } from "react";
 import {
   LightModeOutlined,
   DarkModeOutlined,
   Menu as MenuIcon,
   Search,
   SettingsOutlined,
+  ArrowDropDownOutlined,
 } from "@mui/icons-material";
 import { FlexBetween } from "./FlexBetween";
 import { useDispatch } from "react-redux";
 import {
   AppBar,
+  Button,
   IconButton,
   InputBase,
   Toolbar,
   useTheme,
+  Box,
+  Typography,
+  Menu,
+  MenuItem,
 } from "@mui/material";
 import { setMode } from "../redux";
 import { IThemeSettings, IUser } from "../interfaces";
+import profile from "../assets/profile.png";
 
 interface IProps {
   isSidebarOpen: boolean;
@@ -24,9 +31,20 @@ interface IProps {
   user: IUser;
 }
 
-export const NavBar: FC<IProps> = ({ isSidebarOpen, setIsSidebarOpen }) => {
+export const NavBar: FC<IProps> = ({
+  isSidebarOpen,
+  setIsSidebarOpen,
+  user,
+}) => {
   const dispatch = useDispatch();
   const theme: IThemeSettings = useTheme();
+
+  const [anchorEl, setAnchorEl] = useState(null);
+  const isOpen = Boolean(anchorEl);
+
+  const handleClick = (event: { currentTarget: SetStateAction<null> }) =>
+    setAnchorEl(event.currentTarget);
+  const handleClose = () => setAnchorEl(null);
 
   return (
     <AppBar
@@ -68,6 +86,60 @@ export const NavBar: FC<IProps> = ({ isSidebarOpen, setIsSidebarOpen }) => {
           <IconButton>
             <SettingsOutlined />
           </IconButton>
+
+          <FlexBetween>
+            <Button
+              onClick={handleClick}
+              sx={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+                textTransform: "none",
+                gap: "1rem",
+              }}
+            >
+              <Box
+                component={"img"}
+                alt="profile"
+                src={profile}
+                sx={{
+                  objectFit: "cover",
+                  marginRight: "12px",
+                  height: "32px",
+                  width: "32px",
+                  borderRadius: "50%",
+                }}
+              />
+
+              <Box textAlign={"left"}>
+                <Typography
+                  fontWeight={"bold"}
+                  fontSize={"0.85rem"}
+                  sx={{ color: theme.palette.secondary[300] }}
+                >
+                  {user.name}
+                </Typography>
+
+                <Typography
+                  fontSize={"0.75rem"}
+                  sx={{ color: theme.palette.secondary[200] }}
+                >
+                  {user.occupation}
+                </Typography>
+              </Box>
+              <ArrowDropDownOutlined
+                sx={{ color: theme.palette.secondary[300], fontSize: "25px" }}
+              />
+            </Button>
+            <Menu
+              open={isOpen}
+              anchorEl={anchorEl}
+              onClose={handleClose}
+              anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+            >
+              <MenuItem onClick={handleClose}>Log Out</MenuItem>
+            </Menu>
+          </FlexBetween>
         </FlexBetween>
       </Toolbar>
     </AppBar>
